99f6338129aa22d46672cfe05ca30efd249f143f
AsilbekAbdulloyev2010 <abdulloyev570@gmail.com>
2024-09-11 22:39:21 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+ // alert("hello")//
+
+//  console.log("hello console")
+//  //   prompt("What's your name?")
+// //  Boolean
+// console.log(true);
+// console.log(false);
+// // typeof
+//  console.log(hello);
+//  console.log(typeof false);
+//  console.log( 76);
+//  console.log(typeof { name: "Asilbek"})
+// // NULL
+// console.log(typeof null);
+// console.log(typeof undefined);  
+// // O'zgaruvchi (Variable) (var)
+
+//  var hello = "asilbek";
+
+//  console.log(hello)
+//  let name = prompt("What's your name?");
+
+//  console.log(Name) 
+//  let ism = prompt("Ismingiz nima")
+
+//  var abc = "O'zbekiston - vatanim manim"
+//  let telephone = "iPhone 15";
+//  let phone = "Samsung Galaxy";
+//  phone = "Samsung A7";
+
+//  console.log(telephone)
+//  console.log(phone)
+
+
+//  let address = "Ahmad Donish 34-uy";
+
+//  address = "Mevazor 35"
+
+//  console.log(address)
+
+
+//  let userAge;
+
+//  userAge = 14;
+
+//  console.log(userAge);
+// let message = "Bu muhim xabar!";
+// console.log(message);
+// console.error(message);
+// console.table(message);
+// console.warn(message);
+// let son = 3;
+for (let num = "#"; num <= "########"; num += "#") {
+    console.log(num);
+}
+  
+// let user ="Asilbek" 
+
+
+// console.log(user); 
+
+
+// const teacher = "David"; 
+// console.log(teacher);
+
+
+// const username = "Asilbek developer"; 
+// console.log(username)
+// console.log(teacher);
+// console.log("#");
+// console.log("##");
+// console.log("###");
